84fae8077dcacaeb3b0f79c4ccbf221d1817398c
Michael <michaeldayvisonmariano.tech@gmail.com>
2025-09-01 17:54:53 -0400

    Initial commit

---
+++ poi_project/asgi.py
@@ -0,0 +1,10 @@
+
+#ASGI config for poi_project project.
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'poi_project.settings')
+
+application = get_asgi_application()
